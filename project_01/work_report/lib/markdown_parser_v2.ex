a0a0a032b6880e3ba01dc436d1e2adaaa94567f9
dmitry-sidorov <jimmaggot@mail.ru>
2024-12-10 16:49:37 +0300

    Make month name case insensetive

--- project_01/work_report/lib/markdown_parser_v2.ex
+++ project_01/work_report/lib/markdown_parser_v2.ex
@@ -103,19 +103,19 @@ defmodule WorkReport.MarkdownParserV2 do
 
   @spec get_month_number(month_title :: String.t()) :: integer() | parser_error()
   def get_month_number(month_title) do
-    case month_title do
-      "January" -> 1
-      "February" -> 2
-      "March" -> 3
-      "April" -> 4
-      "May" -> 5
-      "June" -> 6
-      "July" -> 7
-      "August" -> 8
-      "September" -> 9
-      "October" -> 10
-      "November" -> 11
-      "December" -> 12
+    case String.downcase(month_title) do
+      "january" -> 1
+      "february" -> 2
+      "march" -> 3
+      "april" -> 4
+      "may" -> 5
+      "june" -> 6
+      "july" -> 7
+      "august" -> 8
+      "september" -> 9
+      "october" -> 10
+      "november" -> 11
+      "december" -> 12
       _ -> {:error, "invalid_month_name", context: month_title}
     end
   end
